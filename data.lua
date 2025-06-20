d8cb9c356d81daafcfc5e805b8dd3a2bae22f688
Matthew McKney <mrmckney@gmail.com>
2025-06-20 18:13:42 -0400

    Update Todo list and data files: mark additional tiers of belts as work in progress and add new data-final-fixes file

--- data.lua
+++ data.lua
@@ -1,32 +1,38 @@
---data.lua
+-- data.lua
+
+-- Mining Drill
 require("prototypes.mining-drills.entities")
 require("prototypes.mining-drills.items")
 require("prototypes.mining-drills.recipes")
 require("prototypes.mining-drills.technology")
 
+-- Electronics
 require("prototypes.electronics.entities")
 require("prototypes.electronics.items")
 require("prototypes.electronics.recipes")
 require("prototypes.electronics.technology")
 
+-- Assembling Machines
 require("prototypes.assembling-machines.corpses")
 require("prototypes.assembling-machines.entities")
 require("prototypes.assembling-machines.items")
 require("prototypes.assembling-machines.recipes")
 require("prototypes.assembling-machines.technology")
 
--- Under construction, vehicles are not yet complete
+-- Vehicles (order issue)
 -- require("prototypes.vehicles.entities")
 -- require("prototypes.vehicles.items")
 -- require("prototypes.vehicles.recipes")
 -- require("prototypes.vehicles.technology")
 
+-- Belts
 require("prototypes.belts.corpses")
 require("prototypes.belts.entities")
 require("prototypes.belts.items")
 require("prototypes.belts.recipes")
 require("prototypes.belts.technology")
 
+-- Splitters
 require("prototypes.splitters.corpses")
 require("prototypes.splitters.entities")
 require("prototypes.splitters.items")
